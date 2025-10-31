0587e77268bb6fe3d62471cc5b4709695bc13435
saimnabeel <Saimnabeeliqbal@gmail.com>
2025-10-31 18:10:51 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+number_of_family_members = 4
+print("Number of family members:", number_of_family_members)
+
+name="saim"
+name2="ahmed"
+name3="anum"
+name4="nabeel"
+ print("Family member names:", name, name2, name3, name4)
+
